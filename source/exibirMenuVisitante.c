763679b4440484ade4c97fbb5c08fab2e4eedf03
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2024-10-06 11:53:42 -0300

    feat: criado a função exibirMenuVisitante

---
+++ source/exibirMenuVisitante.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int exibirMenuVisitante(){
+
+    int menu;
+
+    do
+    {
+        printf("Bem vindo a Livraria!\n");
+        printf("1 - Realizar cadastro\n");
+        printf("2 - Realizar login\n");
+        printf("3 - Sair\n");
+        scanf("%d", &menu);
+
+    }while(menu < 1 || menu > 3);
+
+    return menu;
+}
--- source/main.c
+++ source/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "funcoes.h"
 
 int main(){
+
+    int menu;
+    menu = exibirMenuVisitante();
+    
+
 
     return 0;
 }
