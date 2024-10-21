2423783de5ea047a66d35cd32c2b72def2dbce0c
PedroSchneider1 <162492569+PedroSchneider1@users.noreply.github.com>
2024-10-21 19:47:11 -0300

    fix: corrigida formatação do catalogo

--- source/catalogarLivros.c
+++ source/catalogarLivros.c
@@ -10,26 +10,28 @@ void catalogarLivros(){
 
     ptrArquivo = fopen("livros.txt", "r");
     if(ptrArquivo == NULL){
-        printf("ERRO: O arquivo ainda não existe.\n");
-        exit(1);
+        printf("Ainda nao ha catalogo.\n");
+        return;
     }
 
     printf("\n");
     // Imprime o cabeçalho da tabela
-    printf("%-5s | %-30s | %-20s | %-20s | %-5s | %-10s | %-13s | %-7s\n", 
+    printf("%5s | %-50s | %-50s | %-50s | %-5s | %-10s | %-13s | %-7s\n", 
            "ID", "Titulo", "Autor", "Editora", "Ano", "Quantidade", "Preco", "Status");
-    printf("--------------------------------------------------------------------------------------------------------------------------------\n");
+    printf("--------------------------------------------------------------------------");
+    printf("--------------------------------------------------------------------------");
+    printf("---------------------------------------------------------------\n");
 
     while (fread(&livro_atual, bytes, 1, ptrArquivo) == 1) {
-        // Remove the newline character from the strings
+        // Retira o '\n' das strings
         livro_atual.titulo[strcspn(livro_atual.titulo, "\n")] = '\0';
         livro_atual.autor[strcspn(livro_atual.autor, "\n")] = '\0';
         livro_atual.editora[strcspn(livro_atual.editora, "\n")] = '\0';
-
-        printf("%-5d | %-30s | %-20s | %-20s | %-5d | %-10d | R$ %-10.2f | %-7d\n", 
+        
+        printf("%5d | %-50s | %-50s | %-50s | %-5d | %-10d | R$ %-10.2f | %-7d\n", 
                livro_atual.livroId, livro_atual.titulo, livro_atual.autor, livro_atual.editora, 
                livro_atual.ano, livro_atual.qttEstoque, livro_atual.preco, livro_atual.status);
-        }
+    }
 
     fclose(ptrArquivo);
 }
